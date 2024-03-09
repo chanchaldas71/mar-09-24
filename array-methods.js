2b0a8fc199fa307815eda3b6cebea3e09533bbd4
chanchaldas71 <abhijitdas47514354@gmail.com>
2024-03-09 21:59:33 +0600

    i love Programming hero

---
+++ array-methods.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        name: 'laptop',
+        brand:'Dell',
+        price:32000,
+        color:'Red'
+    }
+    {
+        name: 'laptop',
+        brand:'HP',
+        price:38000,
+        color:'Red'
+    }
+    {
+        name: 'laptop',
+        brand:'apple',
+        price:32000,
+        color:'Red'
+    }
+    {
+        name: 'laptop',
+        brand:'samsung',
+        price:32000,
+        color:'Red'
+    }
+    {
+        name: 'laptop',
+        brand:'Walton',
+        price:32000,
+        color:'Red'
+    }
+    {
+        name: 'laptop',
+        brand:'Nokia',
+        price:32000,
+        color:'Red'
+    }
+]
